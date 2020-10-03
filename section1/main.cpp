a8690aa2f0fe6aa4c2ae55741008f8fd3e162b93
majid <majid@pensasystems.com>
2020-10-03 13:47:55 -0500

    test class

---
+++ section1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Test {
+private:
+    int data;
+public:
+    Test() : data{0} { std::cout << "Test constructor (" << data << ")" << std::endl; }
+    Test(int data) : data {data} { std::cout << "Test constructor (" << data << ")" << std::endl; }
+    int get_data() const {return data; }
+    ~Test() {std::cout << "Test destructor (" << data << ")" << std::endl; }
+};
+
+using namespace std;
+
+int main() {
+    // Test t1 {1000};
+//    Test *t1 = new Test {1000};
+    // delete t1;
+  std::unique_ptr<Test> t1 {new Test{100}};
+  std::unique_ptr<Test> t2 = std::make_unique<Test>(1000);
+
+       return 0;
+}
+
+
