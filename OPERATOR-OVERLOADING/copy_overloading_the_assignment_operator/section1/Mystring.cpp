f99b5e3cd4614a66ab8a57f52cef0987431d0a1a
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-06 18:28:44 -0500

    Overloaded constructor

--- OPERATOR-OVERLOADING/copy_overloading_the_assignment_operator/section1/Mystring.cpp
+++ OPERATOR-OVERLOADING/copy_overloading_the_assignment_operator/section1/Mystring.cpp
@@ -8,3 +8,15 @@ Mystring::Mystring()
     str = new char[1];
     *str = '\0';
 }
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) 
+    : str {nullptr} {
+        if (s==nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[std::strlen(s)+1];
+            std::strcpy(str, s);
+        }
+}
