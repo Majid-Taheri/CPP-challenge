d3449b13ce958bdcb0fc9b7a26691b1309a0f40e
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-07 12:46:28 -0500

    adding  constant

---
+++ RAW-POINTERS/Passing-Pointer-to-a-Function/section3/main_with_const.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void display( const vector<string> *const v) {
+    // (*v).at(0) = "Funny"; //first const would not allow this to compile
+    for (auto str: *v)
+        cout << str << " ";
+    cout << endl; //last const would not allow this to compile
+  
+    // v= nullptr;
+
+int main() {
+
+   cout << "-----------------------------" << endl;
+   vector<string> stooges {"Larry", "Moe", "Curly"};
+   display(&stooges);
+
+    cout  << endl;
+    return 0;
+}
