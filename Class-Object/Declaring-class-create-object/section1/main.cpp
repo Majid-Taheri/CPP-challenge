c7db03d10f6a0b8c4b29d4715da32ff2ea07de08
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-09 16:53:05 -0500

    create a player object

---
+++ Class-Object/Declaring-class-create-object/section1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+class Player {
+    // attributes
+    string name {"Player"};
+    int health {100};
+    int xp {3};
+    
+    // methods
+    void talk(string);
+    bool is_dead();
+};
+
