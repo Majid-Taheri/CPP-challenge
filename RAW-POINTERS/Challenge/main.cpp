b15b187661765de8b30a01bf05a47e33fef006b5
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-08 19:57:11 -0500

    added apply_all func

--- RAW-POINTERS/Challenge/main.cpp
+++ RAW-POINTERS/Challenge/main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int *apply_all (size_t size_array1, vector<int> &v1 , size_t size_array2,  vector<int> &v2) {
+   int *new_storage {nullptr};
+   new_storage = new int[size_array1 * size_array2];   
+   for (size_t i{0}; i <  size_array2 ; ++i){
+       for (size_t j{0}; j <  size_array2 ; ++j) {
+             *(new_storage + i) = v1[i]*v2[j];
+       }
+   }
+   return new_storage;
+}
