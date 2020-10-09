91384a4c32da1a7b1346b0446ce06e9722c8e93d
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-08 20:24:37 -0500

    change vector to array

--- RAW-POINTERS/Challenge/main.cpp
+++ RAW-POINTERS/Challenge/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <vector>
+
 
 using namespace std;
 
-int *apply_all (size_t size_array1, vector<int> &v1 , size_t size_array2,  vector<int> &v2) {
+int *apply_all (size_t size_array1, const int *const array1 , size_t size_array2,  const int *const array2) {
    int *new_storage {nullptr};
-   new_storage = new int[size_array1 * size_array2];   
+   new_storage = new int[size_array1 * size_array2];
+    int position {0};
    for (size_t i{0}; i <  size_array2 ; ++i){
-       for (size_t j{0}; j <  size_array2 ; ++j) {
-             *(new_storage + i) = v1[i]*v2[j];
+       for (size_t j{0}; j <  size_array1 ; ++j) {
+             *(new_storage + i) = array2[i] * array1[j];
+             ++ position;
        }
    }
    return new_storage;
@@ -20,3 +22,4 @@ void print (const int *const array, const size_t &size) {
         cout << array[i] << " ";
     cout << endl;
 }
+
