bf99b21deb836aa8bc8a4e9a6b141fbfcb8976c1
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-08 20:41:22 -0500

    add main section

--- RAW-POINTERS/Challenge/main.cpp
+++ RAW-POINTERS/Challenge/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 // I was unable to pass an array by value or by reference has to use pointer
-int *apply_all (size_t size_array1, const int const *array1 , size_t size_array2, const int const *array2) {
+int *apply_all (const size_t const& size_array1, const int const *array1 , const size_t const &size_array2, const int const *array2) {
    int *new_storage {nullptr};
    new_storage = new int[size_array1 * size_array2];
     int position {0};
@@ -23,3 +23,21 @@ void print (const int *const array, const size_t &size) {
     cout << endl;
 }
 
+int main() {
+    int *my_array {nullptr};
+    size_t size;
+    const int size_array1 {5};
+    const int size_array2 {3};
+ 
+    int array1 [] {1,2,3,4,5};
+    int array2 [] {10,20,30};
+    
+    my_array = apply_all(size_array1, array1, size_array2, array2);
+    cout << "\n--------------------------------------" << endl;
+
+    constexpr size_t result_size {size_array1 * size_array2} ; 
+    
+    print (my_array, result_size);
+    delete [] my_array;
+    return 0;
+}
