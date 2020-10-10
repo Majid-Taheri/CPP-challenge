9774c830a910a798c818b6b4b1997817da1049e1
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-10 13:24:37 -0500

    create pointer that point to the player class

--- Class-Object/Accessing-Class-Members/section1/main.cpp
+++ Class-Object/Accessing-Class-Members/section1/main.cpp
@@ -22,5 +22,12 @@ int main() {
     frank.xp = 12;
     frank.talk("Hi there");
 
+    Player *enemy = new Player ;
+    (*enemy).name = "Enemy";
+    (*enemy).health = 100;
+    enemy->xp = 15;
+    
+    enemy->talk("I will destroy you!");
+
     return 0;
 }
