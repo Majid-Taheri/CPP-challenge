e7cb66dcb01ba7f3e1a60b7a0a46aff63ade25d9
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-12 10:40:46 -0500

    implement main

--- Class-Object/Implementing_Methods/section1/main.cpp
+++ Class-Object/Implementing_Methods/section1/main.cpp
@@ -45,3 +45,27 @@ bool Account::withdraw(double amount) {
         return false;
     }
 }
+
+
+int main() {
+    Account majid_account;
+    majid_account.set_name("Majid's account");
+    majid_account.set_balance(1000.0);
+    
+    if (majid_account.deposit(200.0))
+        cout << "Deposit OK" << endl;
+    else 
+        cout << "Deposit Not allowed" << endl;
+        
+    if (majid_account.withdraw(500.0))
+        cout << "Withdrawal OK" << endl;
+    else
+        cout << "Not sufficient funds" << endl;
+        
+    if (majid_account.withdraw(1500.0))
+        cout << "Withdraw OK" << endl;
+    else
+        cout << "Not sufficient funds" << endl;
+    
+    return 0;
+}
