96d821b5cef91a4639f2ff1072e2ad8eaa14b687
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-12 10:35:52 -0500

    create an account class

---
+++ Class-Object/Implementing_Methods/section1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Account {
+private:
+    // attributes
+    string name;
+    double balance;
+    
+public:
+    // methods
+    // declared inline
+    void set_balance(double bal) { balance = bal; }
+    double get_balance() { return balance; }
+    
+    // methods will be declared outside the class declaration
+    void set_name(string n);
+    string get_name();
+    
+    bool deposit(double amount);
+    bool withdraw(double amount);
+};
