2158f40a6475c69d8882b8c67589a2afbf3d6fe0
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-12 10:39:25 -0500

    implement withdraw method

--- Class-Object/Implementing_Methods/section1/main.cpp
+++ Class-Object/Implementing_Methods/section1/main.cpp
@@ -36,3 +36,12 @@ bool Account::deposit(double amount) {
     balance += amount;
     return true;
 }
+
+bool Account::withdraw(double amount) {
+    if (balance-amount >= 0) {
+        balance -= amount;
+        return true;
+    } else {
+        return false;
+    }
+}
