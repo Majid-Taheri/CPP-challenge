452180bc7da23ec8c4df89c2cfd3ea2a6c982558
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-14 10:41:51 -0500

    implement delegating constructors

--- Class-Object/Delegating_Constructors/section1/main.cpp
+++ Class-Object/Delegating_Constructors/section1/main.cpp
@@ -20,10 +20,16 @@ public:
 //     health = 0;
 // }
 
-Player::Player () 
-    : name {"None"}, health {0}, xp {0} {
+// Player::Player () 
+//     : name {"None"}, health {0}, xp {0} {
 
-    }
+//     }
+
+Player::Player () 
+    :Player {"None" , 0 , 0 } {
+        cout <<"No-args constructor" << endl;
+}
+
 
 // Player::Player (std::string name_val) {
 //     name = name_val;
@@ -31,11 +37,15 @@ Player::Player ()
 //     health = 0;
 // }
 
-Player::Player (std::string name_val) 
-    : name {name_val} , health {0} , xp {0}{
+// Player::Player (std::string name_val) 
+//     : name {name_val} , health {0} , xp {0}{
 
-    }
+// }
 
+Player::Player (std::string name_val) :
+    Player {name_val , 0 ,0 } {
+        cout << "One-arg constructor" << endl;
+}
 
 // Player::Player (std::string name_val , int health_val, int xp_val) {
 //     name = name_val;
@@ -46,7 +56,7 @@ Player::Player (std::string name_val)
 Player::Player (std::string name_val, int health_val, int xp_val)
     : name {name_val} , health {health_val} , xp {xp_val} {
 
-    }
+}
 
 int main () {
     Player empty;
