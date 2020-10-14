81c5bf8efc43c791374ce7c71814f357311e0dbc
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-14 10:37:23 -0500

    copy from constructo_initialization_list

---
+++ Class-Object/Delegating_Constructors/section1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    std::string name {"XXXXXX"};
+    int health;
+    int xp;
+public:
+    Player();
+    Player (std::string name_val);
+    Player (std::string name_val, int health_val, int xp_val);
+}; 
+
+// Player::Player (){
+//     name ="None";
+//     xp = 0;
+//     health = 0;
+// }
+
+Player::Player () 
+    : name {"None"}, health {0}, xp {0} {
+
+    }
+
+// Player::Player (std::string name_val) {
+//     name = name_val;
+//     xp = 0;
+//     health = 0;
+// }
+
+Player::Player (std::string name_val) 
+    : name {name_val} , health {0} , xp {0}{
+
+    }
+
+
+// Player::Player (std::string name_val , int health_val, int xp_val) {
+//     name = name_val;
+//     xp = xp_val;
+//     health = health_val;
+// }
+
+Player::Player (std::string name_val, int health_val, int xp_val)
+    : name {name_val} , health {health_val} , xp {xp_val} {
+
+    }
+
+int main () {
+    Player empty;
+    Player majid {"Majid"};
+    Player hero {"Hero", 100, 11};
+
+    return 0;
+}
