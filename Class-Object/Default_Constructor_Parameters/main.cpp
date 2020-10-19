dd3e6da9a09db5efa68e80643e762e6e6dc7c4a3
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-19 14:45:52 -0500

    implement Player constructor

--- Class-Object/Default_Constructor_Parameters/main.cpp
+++ Class-Object/Default_Constructor_Parameters/main.cpp
@@ -12,3 +12,8 @@ private:
 public:
     Player(std::string name_val ="None", int health_val = 0, int xp_val = 0);
 };
+
+Player::Player(std::string name_val, int health_val, int xp_val) 
+    : name{name_val}, health{health_val}, xp{xp_val} {
+            cout << "Three-args constructor" << endl;
+}
