9aa5cdf3bcb869527fa67c3af1269282479a0078
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-27 20:16:25 -0500

    create Deep class

---
+++ Class-Object/Deep_copy/section1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Deep {
+private:
+    int *data;
+public:
+    void set_data_value(int d) { *data = d; }
+    int get_data_value() { return *data; }
+    // Constructor
+    Deep(int d);
+    // Copy Constructor
+    Deep(const Deep &source);
+    // Destructor
+    ~Deep();
+};
