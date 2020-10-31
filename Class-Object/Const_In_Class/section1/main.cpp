ee01d84a7e2999f16cc1e2c325ec868449ba484d
Majid-Taheri <mjtaheri68@gmail.com>
2020-10-30 21:41:32 -0500

    implement three params constructor

--- Class-Object/Const_In_Class/section1/main.cpp
+++ Class-Object/Const_In_Class/section1/main.cpp
@@ -29,3 +29,7 @@ Player::Player()
 Player::Player(std::string name_val) 
    : Player {name_val,0, 0}  {
 }
+
+Player::Player(std::string name_val, int health_val, int xp_val) 
+    : name{name_val}, health{health_val}, xp{xp_val} {
+}
