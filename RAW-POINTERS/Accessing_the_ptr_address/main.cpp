5af0112175ac242a53b75c6bf16b556657ca03f3
Majid <mjtaheri68@gmail.com>
2021-07-17 17:41:09 -0500

    initial commit

---
+++ RAW-POINTERS/Accessing_the_ptr_address/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num {10};
+    cout<<"Value of num is "<< num << endl;
+    cout << "sizeof num is" << sizeof num << endl;
+    cout << "Address of num is" << &num  << endl;
+
+}
