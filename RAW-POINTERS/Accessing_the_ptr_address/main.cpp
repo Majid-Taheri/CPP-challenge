2a0457e9d6ed10afe53508c4156957f0a57e2aac
Majid <mjtaheri68@gmail.com>
2021-07-17 17:50:21 -0500

    declare 5 pointers and print their size

--- RAW-POINTERS/Accessing_the_ptr_address/main.cpp
+++ RAW-POINTERS/Accessing_the_ptr_address/main.cpp
@@ -18,5 +18,17 @@ int main() {
     cout << "Address of p is" << &p <<endl;
     cout << "Size of p is" << sizeof p << endl;
 
+  ///
+  int *p1 {nullptr};
+  double *p2 {nullptr};
+  unsigned long long *p3 {nullptr};
+  vector<string> *p4 {nullptr};
+  string *p5 {nullptr};
+
+  cout << "\n Size of P1 is " << sizeof p1 << endl;
+  cout << "\n Size of P2 is " << sizeof p2 << endl;
+  cout << "\n Size of P3 is " << sizeof p3 << endl;
+  cout << "\n Size of P4 is " << sizeof p4 << endl;
+  cout << "\n Size of P5 is " << sizeof p5 << endl;
 
 }
