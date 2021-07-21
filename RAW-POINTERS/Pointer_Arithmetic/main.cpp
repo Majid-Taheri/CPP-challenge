96f16d59f5bdf74dc79726eb5abe70255c95ea93
Majid <mjtaheri68@gmail.com>
2021-07-21 18:42:25 -0500

    string comaprision

--- RAW-POINTERS/Pointer_Arithmetic/main.cpp
+++ RAW-POINTERS/Pointer_Arithmetic/main.cpp
@@ -23,6 +23,28 @@ int main (){
         cout << *score_ptr++ << endl;
     }
     
+    cout << "\n----------------------" << endl;
+    string s1 {"Frank"};
+    string s2 {"Frank"};
+    string s3 {"Jmaes"};
+
+    string *p1 {&s1};
+    string *p2 {&s2};
+    string *p3 {&s1};
+
+    cout << boolalpha;
+
+    cout << p1 << " == "  << p2 << " : " << (p1 == p2) << endl;
+    cout << p1 << " == "  << p3 << " : " << (p1 == p3) << endl;
+
+    cout << *p1 << " == "  << *p2 << " : " << (*p1 == *p2) << endl;
+    cout << *p1 << " == "  << *p3 << " : " << (*p1 == *p3) << endl;
+
+
+    p3 = &s3;
+
+    cout << *p1 << " == "  << *p3 << " : " << (*p1 == *p3) << endl;
+
 
     cout << endl;
     return 0;
