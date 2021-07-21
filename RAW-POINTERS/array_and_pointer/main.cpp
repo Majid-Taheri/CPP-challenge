ce2efcce38af7fb56346f7f1615902617423fdd5
Majid <mjtaheri68@gmail.com>
2021-07-21 10:29:29 -0500

    first commit

---
+++ RAW-POINTERS/array_and_pointer/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+   int scores[] {100,95,89};
+
+   cout << "value of scores" << scores << endl;
+
+
+    cout << endl;
+    return 0;
+}
