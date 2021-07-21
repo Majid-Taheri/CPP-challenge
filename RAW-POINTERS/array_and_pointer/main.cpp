cc24f5339e2f25978a0c9ee1780854ad538106ee
Majid <mjtaheri68@gmail.com>
2021-07-21 10:32:09 -0500

    Array offset notation

--- RAW-POINTERS/array_and_pointer/main.cpp
+++ RAW-POINTERS/array_and_pointer/main.cpp
@@ -13,8 +13,13 @@ int main (){
 
    cout << "Value of score_ptr" << scores << endl;
 
+    cout << "\nArray offset notation"  << endl;
 
+    cout << *score_ptr << endl;      //100
 
+    cout << *(score_ptr + 1) << endl;      //95
+
+    cout << *(score_ptr + 2) << endl;      //98
 
     cout << endl;
     return 0;
